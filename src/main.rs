f581ee92a5b5aa2cdbd096c7c6f46f7472a45288
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2023-06-28 23:59:35 -0400

    Add basic Edge struct
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -11,24 +11,43 @@ pub struct Vertex {
   pub point: Point,
 }
 
+#[derive(Debug)]
+pub struct Edge {
+  topo_edge: TopoEdge,
+  // TODO add Curve
+}
+
 pub struct Model {
   next_vertex_id: Id,
-  vertices: Vec<Vertex>,
+  next_edge_id: Id,
+  // TODO add Vec<TopoVertex>, Vec<TopoEdge>
 }
 
 impl Model {
   pub fn new() -> Model {
     Model {
       next_vertex_id: 0,
-      vertices: Vec::new(),
+      next_edge_id: 0,
     }
   }
   pub fn make_vertex(&mut self, point: Point) -> Vertex {
     let id = self.next_vertex_id;
     self.next_vertex_id += 1;
 
-    let topo_vertex = TopoVertex { id };
+    let topo_vertex = TopoVertex { id: VertexId(id) };
     Vertex { topo_vertex, point }
+  }
+
+  pub fn make_chord_edge(&mut self, v0: &Vertex, v1: &Vertex) -> Edge {
+    let id = self.next_edge_id;
+    self.next_edge_id += 1;
+
+    let vertices = EdgeVertices::Chord(v0.topo_vertex.id, v1.topo_vertex.id);
+    let topo_edge = TopoEdge {
+      id: EdgeId(id),
+      vertices,
+    };
+    Edge { topo_edge }
   }
 }
 
@@ -39,6 +58,12 @@ impl std::fmt::Display for Vertex {
       "Vertex{{id: {}, point: {:?}}}",
       self.topo_vertex.id, self.point
     )
+  }
+}
+
+impl std::fmt::Display for Edge {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "Edge{{{}}}", self.topo_edge.id)
   }
 }
 
@@ -57,32 +82,52 @@ fn main() {
 
   let v0 = model.make_vertex(p0);
   let v1 = model.make_vertex(p1);
-  // let chord = TopoEdge::Chord(0, v0, v1);
+  let chord = model.make_chord_edge(&v0, &v1);
 
-  // println!("chord: {}", chord);
-  println!("v0: {}", v0);
-  println!("v1: {}", v1);
+  println!("chord: {}", chord);
 }
 
 // By convention, everything under main will eventually be private/implementation details.
 
+#[derive(Copy, Clone, Debug)]
+struct VertexId(usize);
+#[derive(Copy, Clone, Debug)]
+struct EdgeId(usize);
 type Id = usize;
 
 #[derive(Debug)]
 struct TopoVertex {
-  id: Id,
+  id: VertexId,
 }
 
 #[derive(Debug)]
-enum TopoEdge {
-  // Closed(Id),
-  // Ray(Id, Vertex),
-  Chord(Id, TopoVertex, TopoVertex),
+enum EdgeVertices {
+  // Closed,
+  // Ray( Vertex),
+  Chord(VertexId, VertexId),
+}
+
+#[derive(Debug)]
+struct TopoEdge {
+  id: EdgeId,
+  vertices: EdgeVertices,
 }
 
 // enum TopoFace {
 //   Face(),
 // }
+
+impl std::fmt::Display for VertexId {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "vid: {}", self.0)
+  }
+}
+
+impl std::fmt::Display for EdgeId {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "eid: {}", self.0)
+  }
+}
 
 impl std::fmt::Display for TopoVertex {
   fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
@@ -92,10 +137,12 @@ impl std::fmt::Display for TopoVertex {
 
 impl std::fmt::Display for TopoEdge {
   fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-    match self {
-      // Edge::Closed(id) => write!(f, "ClosedEdge({})", id),
-      // Edge::Ray(id, vertex) => write!(f, "RayEdge({}, {})", id, vertex),
-      TopoEdge::Chord(id, v0, v1) => write!(f, "ChordEdge({}, v0: {}, v1: {})", id, v0, v1),
+    match &self.vertices {
+      // EdgeVertices::Closed => write!(f, "ClosedEdge({})", id),
+      // EdgeVertices::Ray(vertex) => write!(f, "RayEdge({}, {})", id, vertex),
+      EdgeVertices::Chord(vid0, vid1) => {
+        write!(f, "ChordEdge({}, vid0: {}, vid1: {})", self.id, vid0, vid1)
+      }
     }
   }
 }
