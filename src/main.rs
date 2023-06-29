4772424d9b78c7b56210cd5a923774117a91b787
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2023-06-28 22:09:58 -0400

    Add basic data structures (Vertex and Edge)
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+#[derive(Debug)]
+struct Id(usize);
+
+#[derive(Debug)]
+struct Vertex {
+  id: Id,
+}
+
+#[derive(Debug)]
+enum Edge {
+  // Closed(Id),
+  // Ray(Id, Vertex),
+  Chord(Id, Vertex, Vertex),
+}
+
+// enum Face {
+//   Face(),
+// }
+
 fn main() {
-    println!("Hello, world!");
+  let v0 = Vertex { id: Id(0) };
+  let v1 = Vertex { id: Id(1) };
+  let chord = Edge::Chord(Id(0), v0, v1);
+
+  println!("chord: {}", chord);
+}
+
+impl std::fmt::Display for Id {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "id: {}", self.0)
+  }
+}
+
+impl std::fmt::Display for Vertex {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "Vertex{{{}}}", self.id)
+  }
+}
+
+impl std::fmt::Display for Edge {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    match self {
+      // Edge::Closed(id) => write!(f, "ClosedEdge({})", id),
+      // Edge::Ray(id, vertex) => write!(f, "RayEdge({}, {})", id, vertex),
+      Edge::Chord(id, v0, v1) => write!(f, "ChordEdge({}, v0: {}, v1: {})", id, v0, v1),
+    }
+  }
 }
